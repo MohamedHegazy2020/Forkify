0ed69823816ea89400c1ac126ae8903984f754b3
Mohamed Hegazy <48334725+MohamedHegazy2020@users.noreply.github.com>
2025-07-15 19:32:33 +0300

    Add Parcel configuration and update package dependencies

--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -1,6 +1,8 @@
 import View from "./view";
 import icons from "url:../../img/icons.svg";
 
+
+
 class PreviewView extends View {
   _parentElement = '';
 
@@ -8,7 +10,6 @@ class PreviewView extends View {
 
   _generateMarkup() {
     const id = window.location.hash.slice(1);
-    console.log(this._data);
     return `
         <li class="preview">
           <a class="preview__link ${
@@ -21,9 +22,9 @@ class PreviewView extends View {
               <h4 class="preview__title">${this._data.title}</h4>
               <p class="preview__publisher">${this._data.publisher}</p>
               <div class="preview__user-generated ${this._data.key ? '' : 'hidden'}">
-                <svg>
-                  <use href="${icons}#icon-user"></use>
-                </svg>
+              <svg>
+                <use href="${icons}#icon-user"></use>
+              </svg>
               </div>
             </div>
           </a>
